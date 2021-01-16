2f39385a4d433c5b0c59ceaaf8054503ff4d5cee
lakshmi <muthulakshmis@torus.tech>
2021-01-16 12:24:44 +0530

    npm pack updated

--- app/src/app/app.module.ts
+++ app/src/app/app.module.ts
@@ -3,7 +3,8 @@ import { NgModule } from '@angular/core';
 import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 // import { SmlNgCurrencyControlModule } from 'projects/sml-ng-currency-control/src/public-api';
-import { SmlNgCurrencyControlModule } from 'projects/sml-ng-currency-control/src/public-api';
+// import { SmlNgCurrencyControlModule } from 'currency-control';
+import {SmlNgCurrencyControlModule} from 'sml-ng-currency-control';
 
 @NgModule({
   declarations: [
